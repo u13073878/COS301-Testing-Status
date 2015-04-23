0829e281e126ab377ec4f9f6a937e2809990acc9
Frikkie Snyman <frikkie.snyman@live.co.za>
2015-04-23 21:58:28 +0200

    First test for getStatusProfile

--- StatusBTests/getStatusProfile.js
+++ StatusBTests/getStatusProfile.js
@@ -1,1 +1,13 @@
 var status = require('Status');
+
+exports.getStatusForProfileTest = function(test){
+
+	test.expect(1);
+	var result;
+	status.getStatusForProfile("u00000006",function(res){
+		result = res;
+		console.log(res);
+		test.equal(res,0);
+		test.done();
+	});
+}
