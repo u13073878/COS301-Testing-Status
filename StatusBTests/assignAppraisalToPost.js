72ec6b8f599e7f7913ebc23e30d6f946c8cffd09
Frikkie Snyman <frikkie.snyman@live.co.za>
2015-04-23 21:48:17 +0200

    Pulling..

--- StatusBTests/assignAppraisalToPost.js
+++ StatusBTests/assignAppraisalToPost.js
@@ -1,15 +1,7 @@
 var status = require('Status');
 
-// I'm thinking that the testig for this comes from setAppraisal.js, seeing as the team does not have the function names as specified in the
-// master specs
+// // I'm thinking that the testig for this comes from setAppraisal.js, seeing as the team does not have the function names as specified in the
+// // master specs
 
+// Waiting to hear on Chris for exactly what's going on here..
 
-
-exports.setAppraisalTest = function(test){
-	// test.expect(2);
-
-	
-	status.setAppraisal("post1","test");
-	test.equal(1,1);
-	test.done();
-}
