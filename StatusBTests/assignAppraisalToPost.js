6e8774952759cb9972ccdf3e1ed3c31b8efa6fb4
Frikkie Snyman <frikkie.snyman@live.co.za>
2015-04-23 21:47:15 +0200

    Started work on getStatusProfile testing

--- StatusBTests/assignAppraisalToPost.js
+++ StatusBTests/assignAppraisalToPost.js
@@ -2,16 +2,7 @@ var status = require('Status');
 
 // I'm thinking that the testig for this comes from setAppraisal.js, seeing as the team does not have the function names as specified in the
 // master specs
-// exports.activePeriodTest = function(test)
-// {
-// 	test.expect(1);
 
-// 	var expectedResult = JSON.stringify({"active_from":"2015-10-01","active_to":"2015-10-30"});
-// 	var result = status.activePeriod("2015-10-01", "2015-10-30");
-
-// 	test.equal(result, expectedResult);
-// 	test.done();
-// }
 
 
 exports.setAppraisalTest = function(test){
