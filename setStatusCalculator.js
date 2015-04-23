a4694d87fb898ee70576307bde5e012b4c01120b
taariq ghoord <taariqghoord@gmail.com>
2015-04-23 23:11:27 +0200

    added setStatusCalculator test. images to come

---
+++ setStatusCalculator.js
@@ -0,0 +1,20 @@
+
+var test=require("unit.js");
+
+exports.statusCalculatorTest= function(test,num)
+{
+    test.expect(1);
+    
+}
+/*
+ exports.activePeriodTest = function(test)
+ {
+ test.expect(1);
+
+ var expectedResult = JSON.stringify({"active_from":"2015-10-01","active_to":"2015-10-30"});
+ var result = status.activePeriod("2015-10-01", "2015-10-30");
+
+ test.equal(result, expectedResult);
+ test.done();
+ }
+ */
