7fda90dbf9f21d4a1a55a122d4ff49259e94b8ac
Frikkie Snyman <frikkie.snyman@live.co.za>
2015-04-23 22:36:21 +0200

    Added analysis of getStatusProfile testing to tex doc

--- StatusBTests/getStatusProfile.js
+++ StatusBTests/getStatusProfile.js
@@ -1,13 +1,51 @@
+
 var status = require('Status');
 
-exports.getStatusForProfileTest = function(test){
-
+exports.getStatusForProfileTest1 = function(test){
 	test.expect(1);
-	var result;
 	status.getStatusForProfile("u00000006",function(res){
-		result = res;
-		console.log(res);
+		test.equal(res,0);
+		test.done()
+	});
+}
+
+exports.getStatusForProfileTest2 = function(test){
+	test.expect(1);
+	status.getStatusForProfile("u00000001",function(res){
+		test.equal(res,4);
+		test.done();
+	});
+}
+
+exports.getStatusForProfileTest3 = function(test){
+	test.expect(1);
+	status.getStatusForProfile("u00000002",function(res){
+		test.equal(res,8);
+		test.done();
+	});
+}
+
+exports.getStatusForProfileTest4 = function(test){
+	test.expect(1);
+	status.getStatusForProfile("u00000003",function(res){
 		test.equal(res,0);
 		test.done();
 	});
 }
+
+exports.getStatusForProfileTest5 = function(test){
+	test.expect(1);
+	status.getStatusForProfile("u00000004",function(res){
+		test.equal(res,7);
+		test.done();
+	});
+}
+
+exports.getStatusForProfileTest6 = function(test){
+	test.expect(1);
+	status.getStatusForProfile("u00000005",function(res){
+		test.equal(res,2);
+		test.done();
+	});
+}
+	
